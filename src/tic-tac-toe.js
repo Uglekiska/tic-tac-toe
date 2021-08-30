01b6c8fe4f785e449e7a5e1ad01ad5486fea9702
Uglekiska <uglekiska@yandex.by>
2021-08-30 17:59:40 +0300

    fix: update tic-tac-toe file

--- src/tic-tac-toe.js
+++ src/tic-tac-toe.js
@@ -1,34 +1,61 @@
 class TicTacToe {
     constructor() {
-
+        this.CurrentPlayerSymbol = 'x';
+        this.matrixField = [
+            [null, null, null],
+            [null, null, null],
+            [null, null, null]
+        ];
     }
 
-    getCurrentPlayerSymbol() {
-
+    getCurrentPlayerSymbol() { //works
+        return this.CurrentPlayerSymbol;
     }
 
-    nextTurn(rowIndex, columnIndex) {
-
+    nextTurn(rowIndex, columnIndex) { //works
+        if (this.matrixField[rowIndex][columnIndex]) {
+            return;
+        }
+        this.matrixField[rowIndex][columnIndex] = this.CurrentPlayerSymbol;
+        this.CurrentPlayerSymbol = this.CurrentPlayerSymbol === 'x' ? 'o' : 'x';
     }
 
-    isFinished() {
-
+    isFinished() { //works
+        return !!this.getWinner() || this.noMoreTurns();
     }
 
-    getWinner() {
-
+    getWinner() { //nu vrode works kekW
+        if (this.matrixField[0][0] === this.matrixField[0][1] && this.matrixField[0][1] === this.matrixField[0][2]) {
+            return this.matrixField[0][0];
+        } else if(this.matrixField[1][0] === this.matrixField[1][1] && this.matrixField[1][1] === this.matrixField[1][2]) {
+            return this.matrixField[1][0];
+        } else if(this.matrixField[2][0] === this.matrixField[2][1] && this.matrixField[2][1] === this.matrixField[2][2]) {
+            return this.matrixField[2][0];
+        }else if(this.matrixField[0][0] === this.matrixField[1][0] && this.matrixField[1][0] === this.matrixField[2][0]) {
+            return this.matrixField[0][0];
+        }else if(this.matrixField[0][1] === this.matrixField[1][1] && this.matrixField[1][1] === this.matrixField[2][1]) {
+            return this.matrixField[0][1];
+        }else if(this.matrixField[0][2] === this.matrixField[1][2] && this.matrixField[1][2] === this.matrixField[2][2]) {
+            return this.matrixField[0][2];
+        }else if(this.matrixField[0][2] === this.matrixField[1][1] && this.matrixField[1][1] === this.matrixField[2][0]) {
+            return this.matrixField[0][2];
+        }else if(this.matrixField[0][0] === this.matrixField[1][1] && this.matrixField[1][1] === this.matrixField[2][2]) {
+            return this.matrixField[0][0];
+        } else {
+            return null;
+        }
     }
 
-    noMoreTurns() {
-
+    noMoreTurns() { //works
+        return this.matrixField.every(row => row.every(col => col));
     }
 
-    isDraw() {
-
+    isDraw() { //works
+        return !this.getWinner() && this.noMoreTurns();
     }
 
-    getFieldValue(rowIndex, colIndex) {
-
+    getFieldValue(rowIndex, colIndex) { //works
+        return this.matrixField[rowIndex][colIndex];
     }
 }
 
